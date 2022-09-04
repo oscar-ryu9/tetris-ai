46193f69261a3e8fd98fc2b9759511bfc71de805
DaNinjaBoi <ryu.oscar9@gmail.com>
2022-09-04 16:57:27 -0600

    grid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import shapes
-import grid
 
 pygame.init()
 
@@ -15,8 +14,34 @@ logo = pygame.image.load('assets/tetris_logo.jpg')
 pygame.display.set_icon(logo)
 
 
+def draw_grid():
+    grid = pygame.Rect(980, 120, 600, 1200)
+    pygame.draw.rect(screen, (255, 255, 255), grid)
 
-#click window x will quit pygame
+    for i in range(9):
+        start_pos = (((i+1)*60) + 980, 120)
+        end_pos = (((i+1)*60) + 980, 1320)
+        pygame.draw.line(screen, (232, 232, 232), start_pos, end_pos, 2)
+
+    for i in range(19):
+        start_pos = (980, ((i+1)*60) + 120)
+        end_pos = (1580, ((i+1)*60) + 120)
+        pygame.draw.line(screen, (232, 232, 232), start_pos, end_pos, 2)
+
+    pygame.draw.rect(screen, (0, 0, 0), grid, 5)
+
+
+def draw_background():
+    background_color = (255, 255, 255)
+    screen.fill(background_color)
+
+    grey_rectangle1 = pygame.Rect(275, 15, 2010, 1410)
+    grey_rectangle2 = pygame.Rect(280, 20, 2000, 1400)
+    pygame.draw.rect(screen, (207, 207, 207), grey_rectangle1)
+    pygame.draw.rect(screen, (232, 232, 232), grey_rectangle2)
+
+
+#game
 running = True
 while running:
 
@@ -26,19 +51,8 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-    #background
-    background_color = (255, 255, 255)
-    screen.fill(background_color)
-
-    #background aesthetics
-    grey_rectangle1 = pygame.Rect(275, 15, 2010, 1410)
-    grey_rectangle2 = pygame.Rect(280, 20, 2000, 1400)
-    pygame.draw.rect(screen, (207, 207, 207), grey_rectangle1)
-    pygame.draw.rect(screen, (232, 232, 232), grey_rectangle2)
+    draw_background()
+    draw_grid()
     pygame.display.update()
-
-
-
-
 
 pygame.quit()
