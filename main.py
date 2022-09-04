74619bd224f567126ad23fbe88b281fa1e444765
DaNinjaBoi <ryu.oscar9@gmail.com>
2022-09-04 15:38:33 -0600

    first init

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import pygame
+import shapes
 
-print("helo")
+pygame.init()
+
+(width, height) = (2560, 1440)
+screen = pygame.display.set_mode((width, height))
+pygame.display.toggle_fullscreen()
+
+#background
+background_color = (255, 255, 255)
+screen.fill(background_color)
+
+pygame.display.flip()
+
+#tetris caption
+pygame.display.set_caption('Tetris')
+
+#tetris logo
+logo = pygame.image.load('assets/tetris_logo.jpg')
+pygame.display.set_icon(logo)
+
+
+
+# click window x will quit pygame
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+pygame.quit()
