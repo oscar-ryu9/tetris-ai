c537634642d622dc3c4749fd82d2d3d39130e0e5
DaNinjaBoi <ryu.oscar9@gmail.com>
2022-09-04 16:05:51 -0600

    first init

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ pygame.init()
 
 (width, height) = (2560, 1440)
 screen = pygame.display.set_mode((width, height))
-pygame.display.toggle_fullscreen()
-
-#background
-background_color = (255, 255, 255)
-screen.fill(background_color)
-
-pygame.display.flip()
 
 #tetris caption
 pygame.display.set_caption('Tetris')
@@ -22,12 +15,26 @@ pygame.display.set_icon(logo)
 
 
 
-# click window x will quit pygame
+#click window x will quit pygame
 running = True
 while running:
+
+    #exit clause
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+
+    #background
+    background_color = (255, 255, 255)
+    screen.fill(background_color)
+
+    #background aesthetics
+    grey_rectangle1 = pygame.Rect(280, 20, 2000, 1400)
+    grey_rectangle2 = pygame.Rect(285, 25, 1990, 1390)
+    pygame.draw.rect(screen, (207, 207, 207), grey_rectangle1)
+    pygame.draw.rect(screen, (232, 232, 232), grey_rectangle2)
+    pygame.display.update()
+
 
 pygame.quit()
